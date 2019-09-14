595be3de05348fd28d58b1460044ef66fcb71235
ltann <leeroyleeroy18@gmail.com>
2019-09-14 12:07:56 -0700

    finished names

--- names/names.py
+++ names/names.py
@@ -10,11 +10,31 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# # slightly better
+# duplicates = []
+# for name_1 in names_1:
+#     duplicates.append(name_1)
+
+# for name_2 in names_2:
+#     if name_2 in duplicates:
+#         name_2 == name_1
+
+# better
 duplicates = []
+dict_names = {}
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    dict_names[name_1] = True
+
+for name_2 in names_2:
+    if name_2 in dict_names:
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
